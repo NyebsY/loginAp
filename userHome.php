d7fab87602330ca158955227b255494e1991992a
NyebsY <55931668+NyebsY@users.noreply.github.com>
2020-08-08 14:44:53 +0200

    Section 3

--- userHome.php
+++ userHome.php
@@ -15,7 +15,11 @@
 			echo $_SESSION['userName'];
 		?>
 	</h1>
-	<a href= 'logout.php'>Logout</a>
+	
+	<a href = 'profile.php' class = "nav_btn">Profile</a>
+	<a href = 'friends.php' class = "nav_btn">Friends</a>
+	<a href = 'logout.php' class = "nav_btn">Logout</a>
+
 	</form>
 	</center>
 	
