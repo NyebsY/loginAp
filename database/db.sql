2b36e44c0903f61fe4403742c646cc587b3513b5
NyebsY <55931668+NyebsY@users.noreply.github.com>
2020-08-08 14:10:05 +0200

    Update db.sql

--- database/db.sql
+++ database/db.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS `user` (
 `userId` int  NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `surname` varchar(255) NOT NULL,
-  `password` varchar(40) NOT NULL,
+  `password` char(60) NOT NULL,
   `email` varchar(40) NOT NULL,
 	PRIMARY KEY (userId)
 )
